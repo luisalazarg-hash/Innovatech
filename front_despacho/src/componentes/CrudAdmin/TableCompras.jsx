7d13f8c88f2059d3a0de9560321d632a61fd46f0
Yvbmadop <lui.salazarg@duocuc.cl>
2026-05-21 21:42:47 -0400

    fix: corregir IPs locales por la IP publica de AWS

--- front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
@@ -7,7 +7,7 @@ export const TableCompras = () => {
   const [ventas, setVentas] = useState([]);
 
   const compras = async () => {
-    await axios.get("http://192.168.30/api/v1/ventas", {
+    await axios.get("http://54.157.110.95:8081/api/v1/ventas", {
       headers:{
         'Content-Type': 'application/json',
         'Accept': 'application/json'
